0de98ad974f780deb7c6e3d1862404b3cb6d88cc
Aakash Behal <aakashbehal@Aakashs-MacBook-Air.local>
2023-07-19 22:25:13 +0530

    added sass support

--- Container/webpack.config.js
+++ Container/webpack.config.js
@@ -29,7 +29,18 @@ module.exports = {
             {
                 test: /\.(ts|tsx)$/,
                 loader: "ts-loader",
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
         ]
     },
     plugins: [
