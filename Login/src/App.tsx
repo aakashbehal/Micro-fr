3615e89b0bf1752b3631b7296229eb389effca49
Aakash Behal <aakashbehal@Aakashs-MacBook-Air.local>
2023-07-20 12:03:51 +0530

    added Login application

---
+++ Login/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => {
+    return <p>Login Application</p>
+}
+
+export default App
---
+++ Login/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, "src", "index.tsx"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+        port: 3001,
+        open: true
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                loader: "ts-loader",
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "public", "index.html"),
+        }),
+    ],
+}
